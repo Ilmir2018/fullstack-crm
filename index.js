af194651b4d012f61e014c6ef62eccfea8e4c7e2
Ilmir2018 <ilmir2007@inbox.ru>
2021-05-31 12:44:24 +0300

    change3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || '8080'
 
 app.listen(port, () => console.log(`Server has been started ${port}`))
 
